3e00eb856105b78ea2652b2f28aad7696ce8d7d5
MasterFlower <heza@shanghaitech.edu.cn>
2022-05-14 07:26:41 +0800

    Update nCov.js

--- Tasks/nCov.js
+++ Tasks/nCov.js
@@ -56,7 +56,7 @@ const headers = {
     }
     let detail =
         "\n「全国数据」" +
-        "\n\n   -新增境外输入: " +
+        "\n\n    -新增境外输入: " +
         desc.foreignStatistics.suspectedIncr +
         "\n    -现有确诊: " +
         desc.currentConfirmedCount +
